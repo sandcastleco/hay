17e70baf0a9ba32ace4c37302ba3982fdf8664c4
Kevin McGillivray <kevin.mcgillivray@me.com>
2016-06-06 08:24:29 -0500

    Make point class

---
+++ js/Point.js
@@ -0,0 +1,13 @@
+/*
+Class: Point
+Properties:
+Description: Defines an X / Y pixel point on the canvas
+*/
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Point.prototype = (function() {
+
+})();
